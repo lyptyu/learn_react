f837bafd738a977bf5cb9d0a5ca509f98f91b8b1
林怡鹏 <linyipeng@limayao.com>
2022-06-28 09:56:10 +0800

    95

--- src/08-antd-mobile/components/Tabbar.js
+++ src/08-antd-mobile/components/Tabbar.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
 import style from './Tabbar.module.css'
-function Tabbar(props) {
+import {TabBar} from "antd-mobile";
+import {
+  AppOutline,
+  UnorderedListOutline,
+  UserOutline,
+} from 'antd-mobile-icons'
+import {withRouter} from "react-router-dom";
+function kerwinTabbar(props) {
+  const tabs = [
+    {
+      key: '/films',
+      title: '电影',
+      icon: <AppOutline />
+    },
+    {
+      key: '/cinemas',
+      title: '影院',
+      icon: <UnorderedListOutline />
+    },
+    {
+      key: '/Center',
+      title: '我的',
+      icon: <UserOutline />,
+    },
+  ]
   return (
     <div className={style.tabbar}>
-      <ul>
-        <li>
-          <NavLink to="/films" activeClassName={style.kerwinactive}>电影</NavLink>
-        </li>
-        <li>
-          <NavLink to="/cinemas" activeClassName={style.kerwinactive}>影院</NavLink>
-        </li>
-        <li>
-          <NavLink to="/center" activeClassName={style.kerwinactive}>我的</NavLink>
-        </li>
-      </ul>
+      <TabBar onChange={value=>{
+        props.history.push(value)
+      }} activeKey={'/' + props.location.pathname.split('/')[1]}>
+        {tabs.map(item => (
+          <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+        ))}
+      </TabBar>
     </div>
   );
 }
 
-export default Tabbar;
+export default withRouter(kerwinTabbar);
--- src/08-antd-mobile/views/Films.js
+++ src/08-antd-mobile/views/Films.js
@@ -4,7 +4,7 @@ import NowPlaying from "./films/NowPlaying";
 import ComingSoon from "./films/ComingSoon";
 import axios from "axios";
 import style from './css/Flims.module.css'
-import {Swiper} from "antd-mobile";
+import {Swiper,Tabs} from "antd-mobile";
 
 class Films extends Component {
   componentDidMount() {
@@ -27,14 +27,22 @@ class Films extends Component {
             return <Swiper.Item key={index} style={{width:'100%',height:'250px',background: item}}>{item}</Swiper.Item>
           })}
         </Swiper>
-        <ul>
-          <li>
-            <NavLink activeClassName={style.kerwinactive} to="/films/nowplaying">正在热映</NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName={style.kerwinactive} to="/films/commingsoon">即将上映</NavLink>
-          </li>
-        </ul>
+        {/*<ul>*/}
+        {/*  <li>*/}
+        {/*    <NavLink activeClassName={style.kerwinactive} to="/films/nowplaying">正在热映</NavLink>*/}
+        {/*  </li>*/}
+        {/*  <li>*/}
+        {/*    <NavLink activeClassName={style.kerwinactive} to="/films/commingsoon">即将上映</NavLink>*/}
+        {/*  </li>*/}
+        {/*</ul>*/}
+        <div style={{position:'sticky',top:0,background:'white'}}>
+          <Tabs onChange={(value)=>{
+            this.props.history.push(value)
+          }} activeKey={this.props.location.pathname}>
+            <Tabs.Tab title='正在热映' key='/films/nowplaying'></Tabs.Tab>
+            <Tabs.Tab title='即将上映' key='/films/commingsoon'></Tabs.Tab>
+          </Tabs>
+        </div>
         <Switch>
           <Route path='/films/nowplaying' component={NowPlaying}></Route>
           <Route path='/films/commingsoon' component={ComingSoon}></Route>
